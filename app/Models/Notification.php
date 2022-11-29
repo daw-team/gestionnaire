2805a6fc0c92801239e5694a76dde7c9e02e1aa7
AmineZCS <medamineben19@gmail.com>
2022-11-29 22:14:35 +0100

    adding notification model

---
+++ app/Models/Notification.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class absence extends Model
+{
+    use HasFactory;
+    protected $table = 'NOTIFICATION';
+    public const CREATED_AT = null;
+    public const UPDATED_AT = null;
+    public $timestamps = false;
+    protected $primaryKey = 'Num_Not';
+    protected $guarded = []; 
+}
