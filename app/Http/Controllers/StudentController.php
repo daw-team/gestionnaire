3c585600293a6f5bc1b575025f535af5fe905b02
AmineZCS <medamineben19@gmail.com>
2022-11-30 14:54:18 +0100

    restore deleted function

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,6 +9,7 @@ use App\Models\Administrateur;
 use App\Models\enseignant;
 use App\Models\Etudiant;
 use App\Models\module;
+use App\Models\Notification;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
@@ -217,8 +218,21 @@ public function getStudentInfo(request $request) {
         $fileExt = $file->extension();
         $newFile = time() .'.'.$fileExt;
         $filePath = $file->storeAs('public/justifications', $newFile);
-        return Absence::where('Num_Abs', $request->num)
+        $resp =  Absence::where('Num_Abs', $request->num)
                             ->update(['Just_Abs' => "../../../../storage/app/$filePath"]);
+	if($resp != 0){
+		$ens = Absence::select('Num_Ens')
+            ->where('Num_Abs', $request->num)
+            ->get()->value('Num_Ens');
+		$queryState = Notification::insert(['Des_Type' => 'Enseignant','Des_Id' => $ens,'Text_Not' => 'unchecked justification']);
+         if(!$queryState) {
+    return response()->json([
+                'msg' => 'operation failed (inserting notification)',
+         ]);
+}else return 1;  
+	}else return response()->json([
+                'msg' => 'operation failed (uploading justification)',
+         ]);
 }
 
 
